1d215f94ddb5ae71dbd2080fe41653028ecc7e48
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-05-05 01:29:40 -0300

    Terminado el responsive del welcome

--- src/components/welcome/index.jsx
+++ src/components/welcome/index.jsx
@@ -7,55 +7,57 @@ import './index.css'
 
 export const Welcome = () => {
   return (
-    <div className='welcome-container'>
-      <div className="welcome-login">
-        <div className="welcome-login-data">
+    <>
+      <div className='welcome-container'>
+        <div className="welcome-login">
+          <div className="welcome-login-data">
 
-          <img src={twitter} alt="logo twitter" />
+            <img src={twitter} alt="logo twitter" />
 
-          <div className="welcome-login-title">
-            <span>Lo que está pasando ahora</span>
-          </div>
-
-          <div className="welcome-login-subtitle">
-            <span>Únete a TwitDev hoy mismo.</span>
-          </div>
-
-          <div className="welcome-login-button-google">
-            <img src={google} alt="google logo" />
-            <span>Registrarse con Google</span>
-          </div>
-
-          <div className="welcome-login-button-apple">
-            <img src={apple} alt="apple logo" />
-            <span>Registrarse con Apple</span>
-          </div>
-
-          <div className="welcome-login-separation">
-            <div className="line"></div>
-  
-            <span>o</span>
-            <div className="line"></div>
-          </div>
-
-          <div className="welcome-login-button-register">
-            <span>Registrarse con email</span>
-          </div>
-          <div className="welcome-login-terms-and-conditions">
-            <span>Al registrarte, aceptas los Términos de servicio y la Política de privacidad, incluida la política de Uso de Cookies.</span>
-          </div>
-
-          <div className="welcome-login-have-account">
-            <span>¿Ya tienes cuenta?</span>
-          </div>
-            <div className="welcome-login-button-login">
-              <span>Iniciar sesión</span>
+            <div className="welcome-login-title">
+              <span>Lo que está pasando ahora</span>
             </div>
+
+            <div className="welcome-login-subtitle">
+              <span>Únete a TwitDev hoy mismo.</span>
+            </div>
+
+            <div className="welcome-login-button-google">
+              <img src={google} alt="google logo" />
+              <span>Registrarse con Google</span>
+            </div>
+
+            <div className="welcome-login-button-apple">
+              <img src={apple} alt="apple logo" />
+              <span>Registrarse con Apple</span>
+            </div>
+
+            <div className="welcome-login-separation">
+              <div className="line"></div>
+
+              <span>o</span>
+              <div className="line"></div>
+            </div>
+
+            <div className="welcome-login-button-register">
+              <span>Registrarse con email</span>
+            </div>
+            <div className="welcome-login-terms-and-conditions">
+              <span>Al registrarte, aceptas los Términos de servicio y la Política de privacidad, incluida la política de Uso de Cookies.</span>
+            </div>
+
+            <div className="welcome-login-have-account">
+              <span>¿Ya tienes cuenta?</span>
+            </div>
+              <div className="welcome-login-button-login">
+                <span>Iniciar sesión</span>
+              </div>
+          </div>
+        </div>
+        <div className="welcome-image">
+          <img src={twitterWhite} alt='logo twitter'/>
         </div>
       </div>
-      <div className="welcome-image">
-        <img src={twitterWhite} alt='logo twitter' />
-      </div>
-    </div>
+    </>
   )
 }
