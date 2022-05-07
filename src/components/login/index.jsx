323fcb34eb4b532528081500926623361a400bea
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-05-06 23:33:46 -0300

    Finalizadas las intercaces de login y signup

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
+import twitter from '../../images/twitter.svg'
+import google from '../../images/google.svg'
+import apple from '../../images/apple.svg'
+import './index.css'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='signup'>
+      <div className="signup-container">
+        <div className="signup-header">
+          <div className="signup-header-item-1">
+            <button>
+              x
+            </button>
+          </div>
+          <div className="signup-header-item-2">
+            <img src={twitter} alt='twitter logo' />
+          </div>
+          <div className="signup-header-item-3"></div>
+        </div>
+        <div className="signup-data">
+          <div className="welcome-login-subtitle">
+            <span>Inicia sesion en TwitDev</span>
+          </div>
+
+          <div className="welcome-login-button-google">
+              <img src={google} alt="google logo" />
+              <span>Inicia sesion con Google</span>
+            </div>
+
+            <div className="welcome-login-button-apple">
+              <img src={apple} alt="apple logo" />
+              <span>Inicia sesion con Apple</span>
+            </div>
+
+            <div className="welcome-login-separation">
+              <div className="line"></div>
+
+              <span>o</span>
+              <div className="line"></div>
+            </div>
+
+          <div className="login-input">
+            <input type="text" placeholder="Correo electrónico o usuario" />
+            </div>
+          <div className="signup-data-login">
+            <span>¿No tienes una cuenta? <Link to='/signup'>Registrate</Link></span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/signUp/index.jsx
+++ src/components/signUp/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import twitter from '../../images/twitter.svg'
+import google from '../../images/google.svg'
+import apple from '../../images/apple.svg'
 import './index.css'
+import { Link } from 'react-router-dom'
 
 export const SingUp = () => {
   return (
@@ -16,6 +19,38 @@ export const SingUp = () => {
             <img src={twitter} alt='twitter logo' />
           </div>
           <div className="signup-header-item-3"></div>
+        </div>
+        <div className="signup-data">
+          <div className="welcome-login-subtitle">
+            <span>Únete a TwitDev hoy mismo.</span>
+          </div>
+
+          <div className="welcome-login-button-google">
+              <img src={google} alt="google logo" />
+              <span>Registrarse con Google</span>
+            </div>
+
+            <div className="welcome-login-button-apple">
+              <img src={apple} alt="apple logo" />
+              <span>Registrarse con Apple</span>
+            </div>
+
+            <div className="welcome-login-separation">
+              <div className="line"></div>
+
+              <span>o</span>
+              <div className="line"></div>
+            </div>
+
+            <div className="welcome-login-button-register">
+              <span>Registrarse con email</span>
+            </div>
+            <div className="welcome-login-terms-and-conditions">
+              <span>Al registrarte, aceptas los Términos de servicio y la Política de privacidad, incluida la política de Uso de Cookies.</span>
+          </div>
+          <div className="signup-data-login">
+            <span>¿Ya tienes una cuenta? <Link to='/login'>Inicia sesión</Link></span>
+          </div>
         </div>
       </div>
     </div>
